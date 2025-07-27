e337b30aacab2a7f1476fc5795b154a1b0336d66
mraksdev <a.krylov.work@gmail.com>
2025-07-27 21:02:58 +0300

    feat: Add entity class
    - created basic entity class
    - modified player rendering

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import tcod
 
 from actions import EscapeAction, MovementAction
+from entity import Entity
 from input_handlers import EventHandler
 
 def main() -> None:
     screen_width = 80
     screen_height = 50
 
-    player_x = int(screen_width / 2)
-    player_y = int(screen_height / 2)
-
     tileset = tcod.tileset.load_tilesheet("tiles.png", 32, 8, tcod.tileset.CHARMAP_TCOD)
     
     event_handler = EventHandler()
+
+    player = Entity(x=screen_width // 2, y=screen_height // 2, char="@", color=(255, 255, 255))
+    npc = Entity(x=screen_width // 2 - 5, y=screen_height // 2, char="@", color=(255, 255, 0))
+    entities = [player, npc]
 
     with tcod.context.new(
         columns=screen_width, 
@@ -23,11 +25,12 @@ def main() -> None:
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
-            root_console.print(x=player_x, y=player_y, string="@")
+            root_console.clear()
+
+            root_console.print(x=player.x, y=player.y, string=player.char, fg=player.color)
             
             context.present(root_console)
 
-            root_console.clear()
 
             for event in tcod.event.wait():
                 action = event_handler.dispatch(event)
@@ -36,8 +39,7 @@ def main() -> None:
                     continue
                 
                 if isinstance(action, MovementAction):
-                    player_x += action.dx
-                    player_y += action.dy
+                    player.move(action.dx, action.dy)
 
                 if isinstance(action, EscapeAction):
                     raise SystemExit()
